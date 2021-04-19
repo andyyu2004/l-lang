c7243016dbd227317ca8060206b75ec2ff16cacc
Andy Yu <andyyu2004@gmail.com>
2021-04-19 21:57:09 +1200

    comment out traits crate to make it compilable again

--- src/traits/src/interner.rs
+++ src/traits/src/interner.rs
@@ -4,90 +4,89 @@ use std::fmt::{self, Debug, Formatter};
 use std::hash::{Hash, Hasher};
 use std::rc::Rc;
 
-impl<'tcx> Debug for LInterner<'tcx> {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "<interner>")
-    }
-}
+// impl<'tcx> Debug for LInterner<'tcx> {
+//     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+//         write!(f, "<interner>")
+//     }
+// }
 
-// just copy the impl for IRInterner for now
-impl<'tcx> Interner for LInterner<'tcx> {
-    type InternedClause = Rc<ClauseData<Self>>;
-    type InternedClauses = Vec<Clause<Self>>;
-    type InternedGoal = Rc<GoalData<Self>>;
-    type InternedGoals = Vec<Goal<Self>>;
-    type InternedSubsts = Vec<Term<Self>>;
-    type InternedTerm = Rc<TermData<Self>>;
-    type InternedTerms = Vec<Term<Self>>;
+// // just copy the impl for IRInterner for now
+// impl<'tcx> Interner for LInterner<'tcx> {
+//     type InternedClause = Rc<ClauseData<Self>>;
+//     type InternedClauses = Vec<Clause<Self>>;
+//     type InternedGoal = Rc<GoalData<Self>>;
+//     type InternedGoals = Vec<Goal<Self>>;
+//     type InternedTerm = Rc<TermData<Self>>;
+//     type InternedTerms = Vec<Term<Self>>;
 
-    fn clause_data<'a>(&self, clause: &'a Self::InternedClause) -> &'a ClauseData<Self> {
-        clause
-    }
+//     fn clause_data<'a>(&self, clause: &'a Self::InternedClause) -> &'a ClauseData<Self> {
+//         clause
+//     }
 
-    fn clauses<'a>(&self, clauses: &'a Self::InternedClauses) -> &'a [Clause<Self>] {
-        clauses.as_slice()
-    }
+//     fn clauses<'a>(&self, clauses: &'a Self::InternedClauses) -> &'a [Clause<Self>] {
+//         clauses.as_slice()
+//     }
 
-    fn goal_data<'a>(&self, goal: &'a Self::InternedGoal) -> &'a GoalData<Self> {
-        goal
-    }
+//     fn goal_data<'a>(&self, goal: &'a Self::InternedGoal) -> &'a GoalData<Self> {
+//         goal
+//     }
 
-    fn goals<'a>(&self, goals: &'a Self::InternedGoals) -> &'a [Goal<Self>] {
-        goals.as_slice()
-    }
+//     fn goals<'a>(&self, goals: &'a Self::InternedGoals) -> &'a [Goal<Self>] {
+//         goals.as_slice()
+//     }
 
-    fn term_data<'a>(&self, term: &'a Self::InternedTerm) -> &'a TermData<Self> {
-        term
-    }
+//     fn term_data<'a>(&self, term: &'a Self::InternedTerm) -> &'a TermData<Self> {
+//         term
+//     }
 
-    fn terms<'a>(&self, terms: &'a Self::InternedTerms) -> &'a [Term<Self>] {
-        terms.as_slice()
-    }
+//     fn terms<'a>(&self, terms: &'a Self::InternedTerms) -> &'a [Term<Self>] {
+//         terms.as_slice()
+//     }
 
-    fn intern_goal(self, goal: GoalData<Self>) -> Self::InternedGoal {
-        Rc::new(goal)
-    }
+//     fn intern_goal(self, goal: GoalData<Self>) -> Self::InternedGoal {
+//         Rc::new(goal)
+//     }
 
-    fn intern_clause(self, clause: ClauseData<Self>) -> Self::InternedClause {
-        Rc::new(clause)
-    }
+//     fn intern_clause(self, clause: ClauseData<Self>) -> Self::InternedClause {
+//         Rc::new(clause)
+//     }
 
-    fn intern_clauses(
-        self,
-        clauses: impl IntoIterator<Item = Clause<Self>>,
-    ) -> Self::InternedClauses {
-        clauses.into_iter().collect()
-    }
+//     fn intern_clauses(
+//         self,
+//         clauses: impl IntoIterator<Item = Clause<Self>>,
+//     ) -> Self::InternedClauses {
+//         clauses.into_iter().collect()
+//     }
 
-    fn intern_term(self, term: TermData<Self>) -> Self::InternedTerm {
-        Rc::new(term)
-    }
+//     fn intern_term(self, term: TermData<Self>) -> Self::InternedTerm {
+//         Rc::new(term)
+//     }
 
-    fn intern_goals(self, goals: impl IntoIterator<Item = Goal<Self>>) -> Self::InternedGoals {
-        goals.into_iter().collect()
-    }
+//     fn intern_goals(self, goals: impl IntoIterator<Item = Goal<Self>>) -> Self::InternedGoals {
+//         goals.into_iter().collect()
+//     }
 
-    fn intern_terms(self, terms: impl IntoIterator<Item = Term<Self>>) -> Self::InternedTerms {
-        terms.into_iter().collect()
-    }
+//     fn intern_terms(self, terms: impl IntoIterator<Item = Term<Self>>) -> Self::InternedTerms {
+//         terms.into_iter().collect()
+//     }
 
-    fn intern_substs(self, substs: impl IntoIterator<Item = Term<Self>>) -> Self::InternedSubsts {
-        substs.into_iter().collect()
-    }
-}
+//     fn intern_substs(self, substs: impl IntoIterator<Item = Term<Self>>) -> Self::InternedSubsts {
+//         substs.into_iter().collect()
+//     }
+// }
 
-// the following traits are required but never used
-impl<'tcx> Hash for LInterner<'tcx> {
-    fn hash<H: Hasher>(&self, _state: &mut H) {
-        panic!()
-    }
-}
+// // the following traits are required but never used
+// impl<'tcx> Hash for LInterner<'tcx> {
+//     fn hash<H: Hasher>(&self, _state: &mut H) {
+//         panic!()
+//     }
+// }
 
-impl<'tcx> PartialEq for LInterner<'tcx> {
-    fn eq(&self, _other: &Self) -> bool {
-        panic!()
-    }
-}
+// impl<'tcx> PartialEq for LInterner<'tcx> {
+//     fn eq(&self, _other: &Self) -> bool {
+//         panic!()
+//     }
+// }
 
-impl<'tcx> Eq for LInterner<'tcx> {
-}
+// impl<'tcx> Eq for LInterner<'tcx> {
+// }
--- src/traits/src/lib.rs
+++ src/traits/src/lib.rs
@@ -1,61 +1,61 @@
-mod interner;
+// mod interner;
 
-use lcore::ty::{Ty, TyCtx, TyKind};
-use logic_ir::*;
+// use lcore::ty::{Ty, TyCtx, TyKind};
+// use logic_ir::*;
 
-#[derive(Copy, Clone)]
-pub struct LInterner<'tcx> {
-    tcx: TyCtx<'tcx>,
-}
+// #[derive(Copy, Clone)]
+// pub struct LInterner<'tcx> {
+//     tcx: TyCtx<'tcx>,
+// }
 
-impl<'tcx> LInterner<'tcx> {
-    fn tys_to_terms(self, ty: &[Ty<'tcx>]) -> Terms<Self> {
-        Terms::intern(self, ty.iter().map(|ty| self.ty_to_term(ty)))
-    }
+// impl<'tcx> LInterner<'tcx> {
+//     fn tys_to_terms(self, ty: &[Ty<'tcx>]) -> Terms<Self> {
+//         Terms::intern(self, ty.iter().map(|ty| self.ty_to_term(ty)))
+//     }
 
-    // this is probably a really bad idea, just experimenting
-    fn ty_to_term(self, ty: Ty<'tcx>) -> Term<Self> {
-        let mk_atom = |atom| Atom::new(Sym::intern(atom));
-        let mk_term_atom = |atom| Term::intern(self, TermData::Atom(mk_atom(atom)));
-        let mk_structure = |f, terms| Term::intern(self, TermData::Structure(mk_atom(f), terms));
+//     // this is probably a really bad idea, just experimenting
+//     fn ty_to_term(self, ty: Ty<'tcx>) -> Term<Self> {
+//         let mk_atom = |atom| Atom::new(Sym::intern(atom));
+//         let mk_term_atom = |atom| Term::intern(self, TermData::Atom(mk_atom(atom)));
+//         let mk_structure = |f, terms| Term::intern(self, TermData::Structure(mk_atom(f), terms));
 
-        match ty.kind {
-            TyKind::Box(inner) => mk_structure("box", self.tys_to_terms(&[inner])),
-            TyKind::Tuple(tys) => mk_structure("tuple", self.tys_to_terms(tys)),
-            TyKind::FnPtr(fnptr) => todo!(),
-            TyKind::Array(_, _) => todo!(),
-            TyKind::Infer(_) => todo!(),
-            TyKind::Ptr(_) => todo!(),
-            TyKind::Param(_) => todo!(),
-            TyKind::Opaque(_, _) => todo!(),
-            TyKind::Adt(adt, substs) => todo!(),
-            TyKind::Bool => mk_term_atom("bool"),
-            TyKind::Discr => mk_term_atom("discr"),
-            TyKind::Char => mk_term_atom("char"),
-            TyKind::Float => mk_term_atom("float"),
-            TyKind::Int => mk_term_atom("int"),
-            TyKind::Error => mk_term_atom("err"),
-            TyKind::Never => mk_term_atom("never"),
-        }
-    }
-}
+//         match ty.kind {
+//             TyKind::Box(inner) => mk_structure("box", self.tys_to_terms(&[inner])),
+//             TyKind::Tuple(tys) => mk_structure("tuple", self.tys_to_terms(tys)),
+//             TyKind::FnPtr(fnptr) => todo!(),
+//             TyKind::Array(_, _) => todo!(),
+//             TyKind::Infer(_) => todo!(),
+//             TyKind::Ptr(_) => todo!(),
+//             TyKind::Param(_) => todo!(),
+//             TyKind::Opaque(_, _) => todo!(),
+//             TyKind::Adt(adt, substs) => todo!(),
+//             TyKind::Bool => mk_term_atom("bool"),
+//             TyKind::Discr => mk_term_atom("discr"),
+//             TyKind::Char => mk_term_atom("char"),
+//             TyKind::Float => mk_term_atom("float"),
+//             TyKind::Int => mk_term_atom("int"),
+//             TyKind::Error => mk_term_atom("err"),
+//             TyKind::Never => mk_term_atom("never"),
+//         }
+//     }
+// }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use ldriver::Driver;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//     use ldriver::Driver;
 
-    #[test]
-    fn ty_to_term() {
-        Driver::from_src("")
-            .with_tcx(|tcx| {
-                let ty = tcx.mk_ty(TyKind::Int);
-                let ty = tcx.mk_tup_iter(vec![ty, ty, ty].into_iter());
-                dbg!(ty);
-                let interner = LInterner { tcx };
-                let term = interner.ty_to_term(ty);
-                dbg!(term);
-            })
-            .unwrap();
-    }
-}
+//     #[test]
+//     fn ty_to_term() {
+//         Driver::from_src("")
+//             .with_tcx(|tcx| {
+//                 let ty = tcx.mk_ty(TyKind::Int);
+//                 let ty = tcx.mk_tup_iter(vec![ty, ty, ty].into_iter());
+//                 dbg!(ty);
+//                 let interner = LInterner { tcx };
+//                 let term = interner.ty_to_term(ty);
+//                 dbg!(term);
+//             })
+//             .unwrap();
+//     }
+// }
